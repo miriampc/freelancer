7d030692f3ba415e4000755baee725efa086fd1b
Miriam Peralta <miiriampc@gmail.com>
2017-05-10 12:41:01 -0500

    scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
-/*
- * Archivo principal de JS
- */
+window.addEventListener("scroll", function () {
+var currentScroll=window.pageYOffset || document.body.scrollTop;
+ console.log(currentScroll);
+ if (currentScroll > 329) {
+   document.getElementsByTagName('nav')[0].classList.add("nav-container-min");
+   document.getElementById('txtBootstrap').style.fontSize="21px";
+ }else {
+   document.getElementsByTagName('nav')[0].classList.remove("nav-container-min");
+   document.getElementById('txtBootstrap').style.fontSize="28px";
+ }
+
+ if (currentScroll>675 && currentScroll < 1508) {
+   document.getElementById('link-portfolio').classList.add("bg-green-links");
+ }else {
+   document.getElementById('link-portfolio').classList.remove("bg-green-links");
+ }
+ if(currentScroll >= 1508 && currentScroll < 2062 ){
+   document.getElementById('link-about').classList.add("bg-green-links");
+ }else {
+   document.getElementById('link-about').classList.remove("bg-green-links");
+ }
+
+ if(currentScroll >= 2062){
+   document.getElementById('link-contact').classList.add("bg-green-links");
+ }else {
+   document.getElementById('link-contact').classList.remove("bg-green-links");
+ }
+});
